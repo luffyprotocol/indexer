7ab6a0ea6171dff0787391b0fb16654171151c17
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-06-02 23:39:33 +0530

    add everything

--- protocol/tests/luffy-protocol.test.ts
+++ protocol/tests/luffy-protocol.test.ts
@@ -4,33 +4,33 @@ import {
   test,
   clearStore,
   beforeAll,
-  afterAll
-} from "matchstick-as/assembly/index"
-import { BigInt, Address, Bytes } from "@graphprotocol/graph-ts"
-import { BetAmountSet } from "../generated/schema"
-import { BetAmountSet as BetAmountSetEvent } from "../generated/LuffyProtocol/LuffyProtocol"
-import { handleBetAmountSet } from "../src/luffy-protocol"
-import { createBetAmountSetEvent } from "./luffy-protocol-utils"
+  afterAll,
+} from "matchstick-as";
+import { BigInt, Address, Bytes } from "@graphprotocol/graph-ts";
+import { BetAmountSet } from "../generated/schema";
+import { BetAmountSet as BetAmountSetEvent } from "../generated/LuffyProtocol/LuffyProtocol";
+import { handleBetAmountSet } from "../src/luffy-protocol";
+import { createBetAmountSetEvent } from "./luffy-protocol-utils";
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
 
 describe("Describe entity assertions", () => {
   beforeAll(() => {
-    let amount = BigInt.fromI32(234)
-    let newBetAmountSetEvent = createBetAmountSetEvent(amount)
-    handleBetAmountSet(newBetAmountSetEvent)
-  })
+    let amount = BigInt.fromI32(234);
+    let newBetAmountSetEvent = createBetAmountSetEvent(amount);
+    handleBetAmountSet(newBetAmountSetEvent);
+  });
 
   afterAll(() => {
-    clearStore()
-  })
+    clearStore();
+  });
 
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
   test("BetAmountSet created and stored", () => {
-    assert.entityCount("BetAmountSet", 1)
+    assert.entityCount("BetAmountSet", 1);
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
@@ -38,9 +38,9 @@ describe("Describe entity assertions", () => {
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "amount",
       "234"
-    )
+    );
 
     // More assert options:
     // https://thegraph.com/docs/en/developer/matchstick/#asserts
-  })
-})
+  });
+});
